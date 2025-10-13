a2b2b7862787e3721d8becbc31e47c9c08724a2d
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-10-13 17:32:20 +0530

    Done with Create and Delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+    const add = () => {
+      if(inputValue.trim() !== '') {
+        setTodos([...todos, {id: Date.now(), text: inputValue}]);
+        setInputValue('');
+      }
+    }
+
+    const dlt = (id) => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    }
+
+    // useEffect(() => {
+    //   const todos = JSON.parse(localStorage.getItem(todos));
+    // })
+
+    // useEffect(() => {
+    //   localStorage.setItem("todos", todos);
+    // })
 
   return (
     <>
@@ -10,11 +32,31 @@ function App() {
         <div className='flex flex-col sm:flex-row gap-4'> 
           <input 
           type='text'
-          placeholder='Add your to-do'
+          placeholder='Add your todo'
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
           className='flex-1 bg-gray-200 rounded-2xl p-3'
           />  
-          <button className='bg-orange-500 text-white rounded-2xl p-3 px-5 cursor-pointer whitespace-nowrap'>Add</button>
+          <button 
+          className='bg-orange-500 text-white rounded-2xl p-3 px-5 cursor-pointer whitespace-nowrap'
+          onClick={add}
+          >
+          Add</button>
         </div>
+        {/* The map() method is used only on ARRAYS and since the TODOS is an ARRAy we use it here.*/}
+        <ul className='mt-6 space-y-3'>
+          {todos.map((todo) => (
+            <li key={todo.id} 
+            className='flex justify-between items-center bg-gray-100 p-1 rounded-xl'> 
+            <span className='text-gray-800'>{todo.text}</span>
+            <button 
+            className='text-red-500 hover:text-red-700 font-bold sm:text-2xl lg:text-2xl'
+            onClick={() => dlt(todo.id)}
+            >
+            X</button>
+            </li>
+          ))}
+        </ul>
       </div>
       </div>
     </>
